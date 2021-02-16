bdc7b17ef220be7f308392b484822c1d6c57e996
Sérgio Roberto Garcia Rodrigues Filho <sergiogarciabr@hotmail.com>
2021-02-16 10:46:39 -0300

    - Substituição das queries por Dapper: a maior maravilha inventada depois do Todinho!!!!!!!!!!!!

--- Agenda.DAL/Contatos.cs
+++ Agenda.DAL/Contatos.cs
@@ -1,8 +1,10 @@
 ﻿using Agenda.Domain;
+using Dapper;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
+using System.Linq;
 
 namespace Agenda.DAL
 {
@@ -20,16 +22,8 @@ namespace Agenda.DAL
         {
             using (var con = new SqlConnection(_strCon))
             {
-                string sqlCommand = String.Format("Insert into Contato (Id, Nome) values('{0}', '{1}');", contato.Id.ToString(), contato.Nome);
-
-                con.Open();
-
-                using (SqlCommand cmd = new SqlCommand(sqlCommand, con))
-                {
-                    var rollsAfected = cmd.ExecuteNonQuery();
-                }
+                con.Execute("Insert into Contato (Id, Nome) values(@Id, @Nome)", contato);
             }
-
         }
 
         public Contato Obter(Guid id)
@@ -37,24 +31,7 @@ namespace Agenda.DAL
             var resultado = new Contato();
             using (var con = new SqlConnection(_strCon))
             {
-                var sqlCommand = String.Format("select * from contato where Id = '{0}';", id.ToString());
-
-                con.Open();
-
-                using (SqlCommand cmd = new SqlCommand(sqlCommand, con))
-                {
-                    var sqlRead = cmd.ExecuteReader();
-                    sqlRead.Read();
-
-
-                    resultado = new Contato
-                    {
-                        Id = Guid.Parse(sqlRead["Id"].ToString()),
-                        Nome = sqlRead["Nome"].ToString()
-                    };
-                }
-
-                return resultado;
+                return con.QueryFirst<Contato>("select * from contato where Id = @Id", new { Id = id });
             }
         }
 
@@ -65,20 +42,7 @@ namespace Agenda.DAL
 
             using (var con = new SqlConnection(_strCon))
             {
-                con.Open();
-
-                using (SqlCommand cmd = new SqlCommand(sqlCommand, con))
-                {
-                    var sqlRead = cmd.ExecuteReader();
-                    while (sqlRead.Read())
-                    {
-                        contatos.Add(new Contato
-                        {
-                            Id = Guid.Parse(sqlRead["Id"].ToString()),
-                            Nome = sqlRead["Nome"].ToString()
-                        });
-                    }
-                }
+                contatos = con.Query<Contato>(sqlCommand).ToList();
             }
             return contatos;
         }
