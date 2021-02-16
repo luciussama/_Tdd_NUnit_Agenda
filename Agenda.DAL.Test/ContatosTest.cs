9537d127db5f138fb2697f10b488f151f514f23e
Sérgio Roberto Garcia Rodrigues Filho <sergiogarciabr@hotmail.com>
2021-02-15 22:40:12 -0300

    Add project files.

---
+++ Agenda.DAL.Test/ContatosTest.cs
@@ -0,0 +1,79 @@
+﻿using Agenda.Domain;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace Agenda.DAL.Test
+{
+    [TestFixture]
+    public class ContatosTest
+    {
+        Contatos _contatos;
+
+        [SetUp]
+        public void Setup()
+        {
+            _contatos = new Contatos();
+        }
+
+        [Test]
+        public void AdicionarContatoTest()
+        {
+            //Monta
+            var contato = new Contato()
+            {
+                Id = Guid.NewGuid(),
+                Nome = "Oliver"
+            };
+            
+
+            //Executa
+            _contatos.Adicionar(contato);
+
+            Assert.True(true);
+        }
+
+        [Test]
+        public void ObterContatoTest()
+        {
+            //Monta
+            var contato = new Contato()
+            {
+                Id = Guid.NewGuid(),
+                Nome = "Juzé dos burro"
+            };
+            var contatoResultado = new Contato();
+
+            //Executa
+            _contatos.Adicionar(contato);
+
+            contatoResultado = _contatos.Obter(contato.Id);
+            //Verifica
+            Assert.AreEqual(contato.Id, contatoResultado.Id);
+        }
+        [Test]
+        public void ObterTodosOsContatosTest()
+        {
+            //Montar
+            var contato1 = new Contato() { Id = Guid.NewGuid(), Nome = "Maria" };
+            var contato2 = new Contato() { Id = Guid.NewGuid(), Nome = "Zé da porra" };
+
+            //Executa
+            _contatos.Adicionar(contato1); _contatos.Adicionar(contato2);
+            var lstContato = _contatos.ObterTodos();
+
+            var contatoResultado = lstContato.Where(c => c.Id == contato1.Id).FirstOrDefault();
+
+            //Verifica
+            Assert.IsTrue(lstContato.Count > 1);
+            Assert.AreEqual(contato1.Id, contatoResultado.Id);
+            Assert.AreEqual(contato1.Nome, contatoResultado.Nome);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _contatos = null;
+        }
+    }
+}
---
+++ Agenda.DAL/Contatos.cs
@@ -0,0 +1,78 @@
+﻿using Agenda.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Agenda.DAL
+{
+    public class Contatos
+    {
+        string _strCon;
+        SqlConnection _con;
+
+        public Contatos()
+        {
+            _strCon = @"Data Source=.\sqlexpress;Initial Catalog=Agenda;Integrated Security=true";
+            _con = new SqlConnection(_strCon);
+        }
+
+
+        public void Adicionar(Contato contato)
+        {
+            string sqlCommand = String.Format("Insert into Contato (Id, Nome) values('{0}', '{1}');", contato.Id.ToString(), contato.Nome);
+
+            _con.Open();
+
+            using (SqlCommand cmd = new SqlCommand(sqlCommand, _con))
+            {
+                var rollsAfected = cmd.ExecuteNonQuery();
+            }
+            _con.Close();
+        }
+
+        public Contato Obter(Guid id)
+        {
+            var sqlCommand = String.Format("select * from contato where Id = '{0}';", id.ToString());
+
+            _con.Open();
+
+            using (SqlCommand cmd = new SqlCommand(sqlCommand, _con))
+            {
+                var sqlRead = cmd.ExecuteReader();
+                sqlRead.Read();
+
+                var resultado = new Contato
+                {
+                    Id = Guid.Parse(sqlRead["Id"].ToString()),
+                    Nome = sqlRead["Nome"].ToString()
+                };
+                _con.Close();
+
+                return resultado;
+            }
+        }
+
+        public List<Contato> ObterTodos()
+        {
+            var sqlCommand = String.Format("select * from contato");
+            var contatos = new List<Contato>();
+
+            _con.Open();
+
+            using (SqlCommand cmd = new SqlCommand(sqlCommand, _con))
+            {
+                var sqlRead = cmd.ExecuteReader();
+                while (sqlRead.Read())
+                {
+                    contatos.Add(new Contato
+                    {
+                        Id = Guid.Parse(sqlRead["Id"].ToString()),
+                        Nome = sqlRead["Nome"].ToString()
+                    });
+                }
+            }
+            _con.Close();
+            return contatos;
+        }
+    }
+}
