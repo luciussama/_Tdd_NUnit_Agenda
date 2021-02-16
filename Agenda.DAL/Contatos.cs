1afa95290e1aae3d3f91bfb4cd561677b5234155
Sérgio Roberto Garcia Rodrigues Filho <sergiogarciabr@hotmail.com>
2021-02-16 10:28:50 -0300

    - Ajuste da string de conexão e dos métodos de abertura e fechamento de conexão.

--- Agenda.DAL/Contatos.cs
+++ Agenda.DAL/Contatos.cs
@@ -1,6 +1,7 @@
 ﻿using Agenda.Domain;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data.SqlClient;
 
 namespace Agenda.DAL
@@ -8,45 +9,50 @@ namespace Agenda.DAL
     public class Contatos
     {
         string _strCon;
-        SqlConnection _con;
 
         public Contatos()
         {
-            _strCon = @"Data Source=.\sqlexpress;Initial Catalog=Agenda;Integrated Security=true";
-            _con = new SqlConnection(_strCon);
+            _strCon = ConfigurationManager.ConnectionStrings["connection"].ConnectionString;
         }
 
 
         public void Adicionar(Contato contato)
         {
-            string sqlCommand = String.Format("Insert into Contato (Id, Nome) values('{0}', '{1}');", contato.Id.ToString(), contato.Nome);
-
-            _con.Open();
-
-            using (SqlCommand cmd = new SqlCommand(sqlCommand, _con))
+            using (var con = new SqlConnection(_strCon))
             {
-                var rollsAfected = cmd.ExecuteNonQuery();
+                string sqlCommand = String.Format("Insert into Contato (Id, Nome) values('{0}', '{1}');", contato.Id.ToString(), contato.Nome);
+
+                con.Open();
+
+                using (SqlCommand cmd = new SqlCommand(sqlCommand, con))
+                {
+                    var rollsAfected = cmd.ExecuteNonQuery();
+                }
             }
-            _con.Close();
+
         }
 
         public Contato Obter(Guid id)
         {
-            var sqlCommand = String.Format("select * from contato where Id = '{0}';", id.ToString());
-
-            _con.Open();
-
-            using (SqlCommand cmd = new SqlCommand(sqlCommand, _con))
+            var resultado = new Contato();
+            using (var con = new SqlConnection(_strCon))
             {
-                var sqlRead = cmd.ExecuteReader();
-                sqlRead.Read();
+                var sqlCommand = String.Format("select * from contato where Id = '{0}';", id.ToString());
 
-                var resultado = new Contato
+                con.Open();
+
+                using (SqlCommand cmd = new SqlCommand(sqlCommand, con))
                 {
-                    Id = Guid.Parse(sqlRead["Id"].ToString()),
-                    Nome = sqlRead["Nome"].ToString()
-                };
-                _con.Close();
+                    var sqlRead = cmd.ExecuteReader();
+                    sqlRead.Read();
+
+
+                    resultado = new Contato
+                    {
+                        Id = Guid.Parse(sqlRead["Id"].ToString()),
+                        Nome = sqlRead["Nome"].ToString()
+                    };
+                }
 
                 return resultado;
             }
@@ -57,21 +63,23 @@ namespace Agenda.DAL
             var sqlCommand = String.Format("select * from contato");
             var contatos = new List<Contato>();
 
-            _con.Open();
-
-            using (SqlCommand cmd = new SqlCommand(sqlCommand, _con))
+            using (var con = new SqlConnection(_strCon))
             {
-                var sqlRead = cmd.ExecuteReader();
-                while (sqlRead.Read())
+                con.Open();
+
+                using (SqlCommand cmd = new SqlCommand(sqlCommand, con))
                 {
-                    contatos.Add(new Contato
+                    var sqlRead = cmd.ExecuteReader();
+                    while (sqlRead.Read())
                     {
-                        Id = Guid.Parse(sqlRead["Id"].ToString()),
-                        Nome = sqlRead["Nome"].ToString()
-                    });
+                        contatos.Add(new Contato
+                        {
+                            Id = Guid.Parse(sqlRead["Id"].ToString()),
+                            Nome = sqlRead["Nome"].ToString()
+                        });
+                    }
                 }
             }
-            _con.Close();
             return contatos;
         }
     }
