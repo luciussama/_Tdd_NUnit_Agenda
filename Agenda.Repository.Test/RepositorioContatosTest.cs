c66022a9fd7a76ac32066d346d53032f7b18a587
Sérgio Roberto Garcia Rodrigues Filho <sergiogarciabr@hotmail.com>
2021-02-20 15:02:55 -0300

    - Preparação de mock e testes de RepositorioContatos

---
+++ Agenda.Repository.Test/RepositorioContatosTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Agenda.Domain;
+using Agenda.Repository;
+using NUnit.Framework;
+using Moq;
+using Agenda.DAL;
+
+namespace Agenda.Repository.Test
+{
+    [TestFixture]
+    public class RepositorioContatosTest
+    {
+        Mock<IContatos> _contatos;
+        Mock<ITelefones> _telefones;
+
+        RepositorioContatos _repositorioContatos;
+        
+        [SetUp]
+        public void Setup() {
+            _contatos = new Mock<IContatos>();
+            _telefones = new Mock<ITelefones>();
+            _repositorioContatos = new RepositorioContatos(_contatos.Object, _telefones.Object);
+        }
+
+        [Test]
+        public void Test()
+        {
+
+        }
+        
+        
+        [TearDown]
+        public void TearDown()
+        {
+            _contatos = null;
+            _telefones = null;
+            _repositorioContatos = null;
+        }
+    }
+}
